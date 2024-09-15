842a8d00cc9cf0edaf2c9f523a21ea82e568cae3
Aryan <119997795+Aryan2556@users.noreply.github.com>
2024-09-15 18:38:48 +0530

    Updated

--- frontend/src/config/config.js
+++ frontend/src/config/config.js
@@ -1,9 +1,9 @@
 const config = {
   development: {
-    backendUrl: "http://localhost:8080/api/v1/routess",
+    backendUrl: "http://localhost:8080/api/v1/dalle",
   },
   production: {
-    backendUrl: "https://devswag.onrender.com/api/v1/dalle",
+    backendUrl: "https://threed-design-tshirt.onrender.com",
   },
 };
 
